63cbc9144ec807096f178241e608f5c3624a9c56
Helena Brito Dias <helenabdias95@gmail.com>
2021-07-15 09:40:38 -0300

    bug na busca_profundidade

--- Trabalho.py
+++ Trabalho.py
@@ -145,9 +145,9 @@ def busca_profundidade_lista(G, s):
                     nivel[aux] = nivel[u] + 1
                     marcado[aux] = True
                 break
-            if desempilhar:
-                S.pop()
-    #print(R)
+        if desempilhar:
+            S.pop()
+    print(R)
     print("Busca por profundidade:")
     for j in range(len(G)):
         if (nivel[j] != None):
@@ -196,14 +196,14 @@ def componentes_conexos_lista(G):
 #nome = input("Digite o nome do arquivo: ")
 
 #Entrada de dados
-dados = recebe_dados('teste.txt')
+dados = recebe_dados('collaboration_graph.txt')
 vertice = dados[0]
 aresta = dados[1]
 G = dados[2]
 #isMatriz = int(input("Tipo de Representação: \n1- Matriz \n2-Lista de Adjacencia: "))
-informacoes(G, vertice, aresta)
+#informacoes(G, vertice, aresta)
 busca_profundidade_lista(G, 0)
 busca_largura_lista(G, 0)
-componentes_conexos_lista(G)
+#componentes_conexos_lista(G)
 
 
