69e68a72967658142dc2f3440e232ff171c9e96a
Helena Brito Dias <helenabdias95@gmail.com>
2021-07-22 19:31:56 -0300

    Criação do menu e clean code

--- Trabalho.py
+++ Trabalho.py
@@ -1,33 +1,11 @@
-def recebe_dados (nome):
-    #Receber os dados
-
-    #Recebe o cabeçalho com numero de vertiarquivo = open(nome, 'r')
+def recebe_dados(nome, rep):
     arquivo = open(nome, 'r')
     linha = arquivo.readline()
     listaVA = linha.split(' ')
     num_vertice = int(listaVA[0])
     num_aresta = int(listaVA[1])
-    G = representacao(1, arquivo, num_vertice, num_aresta)
+    G = representacao(rep, arquivo, num_vertice, num_aresta)
 
-    vertice = [i for i in range(num_vertice)]
-    aresta = []
-    """for j in range(num_aresta):
-        linha = arquivo.readline()
-        linha = linha.split(' ')
-        origem = int(linha[0])
-        destino = int(linha[1])
-        peso = int(linha[2])
-        listaAdj[origem].append((destino, peso))
-        listaAdj[destino].append((origem, peso))
-        matAdj[origem][destino] = peso
-        matAdj[destino][origem] = peso
-        aresta.append((origem, destino, peso))
-        aresta.append((destino, origem, peso))"""
-
-    #print(listaAdj)
-    #print(matAdj)
-    #print(vertice)
-    #print(aresta)
     arquivo.close()
 
     return num_vertice, num_aresta, G
@@ -44,8 +22,6 @@ def representacao(isMatriz, arquivo, num_vertice, num_aresta):
             peso = int(linha[2])
             matAdj[origem][destino] = peso
             matAdj[destino][origem] = peso
-            #aresta.append((origem, destino, peso))
-            #aresta.append((destino, origem, peso))
         return matAdj
     else:
         listaAdj = [[] for i in range(num_vertice)]
@@ -57,12 +33,11 @@ def representacao(isMatriz, arquivo, num_vertice, num_aresta):
             peso = int(linha[2])
             listaAdj[origem].append((destino, peso))
             listaAdj[destino].append((origem, peso))
-            #aresta.append((origem, destino, peso))
-            #aresta.append((destino, origem, peso))
         return listaAdj
 
+
 def informacoes_lista(G, vertices, arestas):
-    #Informações lista
+    # Informações lista
     vMaior = G[0]
     vMenor = G[0]
     maior = 0
@@ -83,13 +58,14 @@ def informacoes_lista(G, vertices, arestas):
     print(f"Menor grau: {menor} - vertice:  {vMenor}")
     print(f"Grau médio : {grauMedio}")
     print(f"Frequencia relativa: ")
-    for n in range(menor, (maior+1)):
-        print(f"Grau {n}: {grau.count(n)/vertices}")
+    for n in range(menor, (maior + 1)):
+        print(f"Grau {n}: {grau.count(n) / vertices}")
+
 
 def informacoes_matriz(G, vertices, arestas):
-    #Informações matriz
+    # Informações matriz
     cont = 0
-    while(cont == 0):
+    while cont == 0:
         for i in range(vertices):
             for j in range(vertices):
                 if G[i][j] != 0:
@@ -116,8 +92,10 @@ def informacoes_matriz(G, vertices, arestas):
     print(f"Menor grau: {menor} - vertice:  {vMenor}")
     print(f"Grau médio : {grauMedio}")
     print(f"Frequencia relativa: ")
-    for n in range(menor, (maior+1)):
-        print(f"Grau {n}: {grau.count(n)/vertices}")
+    for n in range(menor, (maior + 1)):
+        print(f"Grau {n}: {grau.count(n) / vertices}")
+
+
 def busca_largura_matriz(G, s):
     desc = [0 for i in range(len(G))]
     nivel = [None] * len(G)
@@ -133,16 +111,17 @@ def busca_largura_matriz(G, s):
                 R.append(v)
                 desc[v] = 1
                 nivel[v] = nivel[u] + 1
-    #print(R)
+    # print(R)
     print("Busca largura: ")
     for j in range(len(G)):
-        if (nivel[j] != None):
+        if nivel[j] is not None:
             print(f"{j}: {nivel[j]}")
     arquivo = open('busca_largura_matriz.txt', 'w')
     arquivo.write("vertice:nivel\n")
     for j in range(len(G)):
-        if (nivel[j] != None):
+        if nivel[j] is not None:
             arquivo.write(f"{j}: {nivel[j]}\n")
+
 
 def busca_largura_lista(G, s):
     desc = [0 for i in range(len(G))]
@@ -163,13 +142,14 @@ def busca_largura_lista(G, s):
     # print(R)
     print("Busca largura: ")
     for j in range(len(G)):
-        if (nivel[j] != None):
+        if nivel[j] is not None:
             print(f"{j}: {nivel[j]}")
     arquivo = open('busca_largura_lista.txt', 'w')
     arquivo.write("vertice:nivel\n")
     for j in range(len(G)):
-        if (nivel[j] != None):
+        if nivel[j] is not None:
             arquivo.write(f"{j}: {nivel[j]}\n")
+
 
 def busca_profundidade_matriz(G, s):
     desc = [0 for i in range(len(G))]
@@ -191,16 +171,17 @@ def busca_profundidade_matriz(G, s):
                 break
         if desempilhar:
             S.pop()
-    #print(R)
+    # print(R)
     print("Busca por profundidade:")
     for j in range(len(G)):
-        if (nivel[j] != None):
+        if nivel[j] is not None:
             print(f"{j}: {nivel[j]}")
     arquivo = open('busca_profundidade_matriz.txt', 'w')
     arquivo.write("vertice:nivel\n")
     for j in range(len(G)):
-        if (nivel[j] != None):
+        if nivel[j] is not None:
             arquivo.write(f"{j}: {nivel[j]}\n")
+
 
 def busca_profundidade_lista(G, s):
     desc = [0 for i in range(len(G))]
@@ -223,23 +204,24 @@ def busca_profundidade_lista(G, s):
                 break
         if desempilhar:
             S.pop()
-    #print(R)
+    # print(R)
     print("Busca por profundidade:")
     for j in range(len(G)):
-        if (nivel[j] != None):
+        if nivel[j] is not None:
             print(f"{j}: {nivel[j]}")
     arquivo = open('busca_profundidade_lista.txt', 'w')
     arquivo.write("vertice:nivel\n")
     for j in range(len(G)):
-        if (nivel[j] != None):
+        if nivel[j] is not None:
             arquivo.write(f"{j}: {nivel[j]}\n")
 
+
 def busca_profundidade_comp_conexa_lista(G, s, marca):
+    comp[s] = marca
     desc = [0 for i in range(len(G))]
     S = [s]
     R = [s]
     desc[s] = 1
-    comp[s] = marca
     while len(S) != 0:
         u = S[-1]
         desempilhar = True
@@ -276,6 +258,7 @@ def busca_profundidade_comp_conexa_matriz(G, s, marca):
         if desempilhar:
             S.pop()
 
+
 def componentes_conexos_lista(G):
     global comp
     comp = [0 for i in range(len(G))]
@@ -285,13 +268,12 @@ def componentes_conexos_lista(G):
         if comp[u] == 0:
             marca += 1
             busca_profundidade_comp_conexa_lista(G, u, marca)
-    #Quantidade de componentes conexas = marca
-    #Varredura da lista de componentes para separação dos grupos
     n = max(comp)
     print(f"Componentes conexas : {marca}")
-    for j in range(1,(n+1)):
+    for j in range(1, (n + 1)):
         print(f"{j}: {comp.count(j)} vertices")
-    #print(comp)
+    # print(comp)
+
 
 def componentes_conexos_matriz(G):
     global comp
@@ -302,31 +284,72 @@ def componentes_conexos_matriz(G):
         if comp[u] == 0:
             marca += 1
             busca_profundidade_comp_conexa_matriz(G, u, marca)
-    #Quantidade de componentes conexas = marca
-    #Varredura da lista de componentes para separação dos grupos
     n = max(comp)
     print(f"Componentes conexas : {marca}")
-    for j in range(1,(n+1)):
+    for j in range(1, (n + 1)):
         print(f"{j}: {comp.count(j)} vertices")
-    #print(comp)
+    # print(comp)
 
-#if __name__ == 'Trabalho':
 
-#nome = input("Digite o nome do arquivo: ")
+# -----------------------------------------------------------------------#
 
-#Entrada de dados
-dados = recebe_dados('teste.txt')
+# MENU
+
+nome = input("Digite o nome do arquivo: ")
+rep = int(input("Tipo de Representação: \n1. Matriz \n2. Lista de Adjacencia: "))
+while (rep != 1 and rep != 2):
+    print("Opção inválida")
+    rep = int(input("Tipo de Representação: \n1. Matriz \n2. Lista de Adjacencia: "))
+
+# Entrada de dados
+dados = recebe_dados(nome, rep)
 vertice = dados[0]
 aresta = dados[1]
 G = dados[2]
-#isMatriz = int(input("Tipo de Representação: \n1- Matriz \n2-Lista de Adjacencia: "))
-"""informacoes_lista(G, vertice, aresta)
-busca_profundidade_lista(G, 0)"""
-#busca_largura_lista(G, 0)
-#componentes_conexos_lista(G)
-#informacoes_matriz(G, vertice, aresta)
-#busca_largura_matriz(G, 0)
-#busca_profundidade_matriz(G, 0)
-componentes_conexos_matriz(G)
 
-
+# Menu de opções para o usuário
+ans = True
+if (rep == 1):
+    while (ans):
+        print("""
+        1. Informações
+        2. Busca por Largura
+        3. Busca por Profundidade
+        4. Componentes Conexos
+        5. Sair
+        """)
+        op = int(input("Qual operação deseja realizar? "))
+        if (op == 1):
+            informacoes_matriz(G, vertice, aresta)
+        elif (op == 2):
+            v = int(input("Informe o vértice de partida: "))
+            busca_largura_matriz(G, v)
+        elif (op == 3):
+            v = int(input("Informe o vértice de partida: "))
+            busca_profundidade_matriz(G, v)
+        elif (op == 4):
+            componentes_conexos_matriz(G)
+        elif (op == 5):
+            ans = False
+elif rep == 2:
+    while ans:
+        print("""
+        1. Informações
+        2. Busca por Largura
+        3. Busca por Profundidade
+        4. Componentes Conexos
+        5. Sair
+        """)
+        op = int(input("Qual operação deseja realizar? "))
+        if op == 1:
+            informacoes_lista(G, vertice, aresta)
+        elif op == 2:
+            v = int(input("Informe o vértice de partida: "))
+            busca_largura_lista(G, v)
+        elif op == 3:
+            v = int(input("Informe o vértice de partida: "))
+            busca_profundidade_lista(G, v)
+        elif op == 4:
+            componentes_conexos_lista(G)
+        elif (op == 5):
+            ans = False
